b907a955f0c9cd8de822a685376bef83b786928a
vadzim_tomas <77vadimtomas77@gmail.com>
2024-04-18 15:16:01 +0300

    some refactor 2

--- buildSrc/src/main/groovy/ReadWriteTaskPlugin.groovy
+++ buildSrc/src/main/groovy/ReadWriteTaskPlugin.groovy
@@ -5,14 +5,16 @@ class ReadWriteTaskPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-         def writeTask = project.tasks.register("write-task", WriteTask) {
+        def writeTask = project.tasks.register("write-task", WriteTask) {
             group = "custom"
             outputFile.set(project.layout.projectDirectory.file("result.txt"))
         }
 
-       def readTask = project.tasks.register("read-task", ReadTask) {
+        def readTask = project.tasks.register("read-task", ReadTask) {
             group = "custom"
             inputFile.set(writeTask.get().outputFile)
         }
+
+        readTask.configure {onlyIf {writeTask.get().didWork}}
     }
 }
