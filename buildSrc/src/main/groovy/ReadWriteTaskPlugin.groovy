83a0b366426c2c4408f783d57b922745eb5c7c82
Anex <77vadimtomas77@gmail.com>
2024-04-23 21:50:05 +0300

    some changes 2

--- buildSrc/src/main/groovy/ReadWriteTaskPlugin.groovy
+++ buildSrc/src/main/groovy/ReadWriteTaskPlugin.groovy
@@ -7,15 +7,13 @@ class ReadWriteTaskPlugin implements Plugin<Project> {
     void apply(Project project) {
         def writeTask = project.tasks.register("write-task", WriteTask) {
             group = "custom"
+            inputFiles.setFrom(project.tasks.named("compileJava").get().outputs.files)
             outputFile.set(project.layout.projectDirectory.file("result.txt"))
-            enabled = false
         }
 
         def readTask = project.tasks.register("read-task", ReadTask) {
             group = "custom"
             inputFile.set(writeTask.get().outputFile)
         }
-
-        readTask.configure {onlyIf {writeTask.get().didWork}}
     }
 }
--- buildSrc/src/main/groovy/WriteTask.groovy
+++ buildSrc/src/main/groovy/WriteTask.groovy
@@ -1,18 +1,26 @@
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 
 @CacheableTask
 class WriteTask extends DefaultTask {
+
+    @InputFiles
+    @PathSensitive(value = PathSensitivity.RELATIVE)
+    final ConfigurableFileCollection inputFiles = project.objects.fileCollection()
 
     @OutputFile
     final RegularFileProperty outputFile = project.objects.fileProperty()
 
     @TaskAction
     void write() {
-        project.layout.projectDirectory.asFileTree.each { File it ->
+        inputFiles.asFileTree.each { File it ->
             outputFile.get().asFile.append("${it.name}\n")
         }
     }
